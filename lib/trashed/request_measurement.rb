de4234259ed64d922e7dd6300b4f4bc1efebb2ef
Jeremy Kemper <jeremy@bitsweat.net>
2008-06-17 23:21:02 -0700

    Include request status and uri on the stats line for easy tracking

--- lib/trashed/request_measurement.rb
+++ lib/trashed/request_measurement.rb
@@ -1,26 +1,25 @@
 module Trashed
   module RequestMeasurement
+    LOG_MESSAGE = 'STATS: %s | %s [%s]'.freeze
+
     def self.included(base)
       base.send :around_filter, :measure_resource_usage
     end
 
     protected
       def measure_resource_usage
-        before = Measurement.measure
+        before = Measurement.now
         yield
       ensure
-        change = Measurement.change_since(before)
-        Rails.logger.info "STATS: #{change.pp}"
+        change = Measurement.now - before
+        Rails.logger.info(LOG_MESSAGE % [change.to_s,
+          headers['Status'].to_s, (complete_request_uri rescue 'unknown')])
       end
 
     class Measurement < Struct.new(:time, :memory, :objects, :gc_runs, :gc_time)
       PP_FORMAT = '%d ms, %.2f KB, %d obj, %d GCs in %d ms'.freeze
 
-      def self.change_since(before)
-        measure - before
-      end
-
-      def self.measure
+      def self.now
         new(Time.now.to_f,
           GC.allocated_size, ObjectSpace.allocated_objects,
           GC.collections, GC.time)
@@ -32,7 +31,7 @@ module Trashed
           gc_runs - other.gc_runs, gc_time - other.gc_time)
       end
 
-      def pp
+      def to_s
         PP_FORMAT % [time * 1000,
           memory / 1024.0, objects,
           gc_runs, gc_time / 1000.0]
