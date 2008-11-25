21abfb9069abffc695146f10391d8d712072aeb8
Jeremy Kemper <jeremy@bitsweat.net>
2008-11-25 15:23:57 -0800

    More complete GC feature check

--- init.rb
+++ init.rb
@@ -1,20 +1,9 @@
 require 'action_controller/dispatcher'
 require 'trashed/measurement'
 
-has_adymo = GC.respond_to?(:enable_stats)
-has_lloyd = GC.respond_to?(:heap_info)
+if Trashed.available?
+  Trashed.enable
 
-if has_adymo || has_lloyd
-  if has_adymo
-    GC.enable_stats
-    Trashed::Measurement = Trashed::AdymoMeasurement
-    Rails.logger.info 'STATS: time, memory, objects, GC runs, GC time (adymo patch)'
-  else
-    Trashed::Measurement = Trashed::LloydMeasurement
-    Rails.logger.info 'STATS: time, heap size, max heap size, GC runs (lloyd patch)'
-  end
-
-  Trashed::Measurement.mark!
   class ActionController::Dispatcher
     before_dispatch { Trashed::Measurement.mark! }
     after_dispatch  { Trashed::Measurement.log! }
--- lib/trashed/measurement.rb
+++ lib/trashed/measurement.rb
@@ -1,4 +1,17 @@
 module Trashed
+  class << self
+    def available?
+      AdymoMeasurement.available? || LloydMeasurement.available?
+    end
+
+    def enable
+      klass = AdymoMeasurement.available? ? AdymoMeasurement : LloydMeasurement
+      const_set :Measurement, klass
+      klass.enable
+      klass.mark!
+    end
+  end
+
   module Benchmarker
     def mark!; @mark = now end
     def delta; now - @mark end
@@ -8,6 +21,17 @@ module Trashed
   class AdymoMeasurement < Struct.new(:time, :memory, :objects, :gc_runs, :gc_time)
     extend Benchmarker
     FORMAT = 'STATS: %d ms, %.2f KB, %d obj, %d GCs in %d ms'.freeze
+
+    def self.available?
+      ObjectSpace.respond_to?(:allocated_objects) &&
+        %w(enable_stats allocated_size allocated_objects collections time).
+        all? { |m| GC.respond_to?(m) }
+    end
+
+    def self.enable
+      GC.enable_stats
+      Rails.logger.info 'STATS: time, memory, objects, GC runs, GC time (adymo patch)'
+    end
 
     def self.now
       new(Time.now.to_f, GC.allocated_size, ObjectSpace.allocated_objects, GC.collections, GC.time)
@@ -29,6 +53,14 @@ module Trashed
   class LloydMeasurement < Struct.new(:time, :memory, :max_memory, :gc_runs)
     extend Benchmarker
     FORMAT = 'STATS: %d ms, %.2f KB, %.2f KB max, %d GCs'.freeze
+
+    def self.available?
+      GC.respond_to?(:heap_info)
+    end
+
+    def self.enable
+      Rails.logger.info 'STATS: time, heap size, max heap size, GC runs (lloyd patch)'
+    end
 
     def self.now
       info = GC.heap_info
