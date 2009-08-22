6e2bc6fc70257139785a1a1905e56dd5d6675197
Jeremy Kemper <jeremy@bitsweat.net>
2009-08-21 17:43:16 -0700

    NewRelic object allocation samplers

---
+++ lib/trashed/newrelic.rb
@@ -0,0 +1,60 @@
+module Trashed
+  class Sampler < NewRelic::Agent::Sampler
+    def poll
+      stats.record_data_point(sample)
+    end
+
+    protected
+      def stats
+        stats_engine.get_stats(self.class::LABEL, false)
+      end
+  end
+
+  class PointSampler < Sampler
+    def sample
+      measure
+    end
+  end
+
+  class DeltaSampler < Sampler
+    def initialize
+      super
+      mark!
+    end
+
+    def sample
+      old = @mark
+      mark!
+      @mark - old
+    end
+
+    protected
+      def mark!
+        @mark = measure
+      end
+  end
+
+  class LiveObjectsSampler < PointSampler
+    LABEL = 'Custom/Objects/Live'
+
+    def self.available?
+      ObjectSpace.respond_to?(:live_objects)
+    end
+
+    def measure
+      ObjectSpace.live_objects
+    end
+  end
+
+  class AllocatedObjectsSampler < DeltaSampler
+    LABEL = 'Custom/Objects/Allocated'
+
+    def self.available?
+      ObjectSpace.respond_to?(:allocated_objects)
+    end
+
+    def measure
+      ObjectSpace.allocated_objects
+    end
+  end
+end
--- lib/trashed/newrelic_sampler.rb
+++
@@ -1,5 +0,0 @@
-if ObjectSpace.respond_to?(:live_objects) && agent = NewRelic::Agent.instance
-  agent.stats_engine.add_sampled_metric 'Ruby/Live Objects' do |stats|
-    stats.record_data_point ObjectSpace.live_objects
-  end
-end
