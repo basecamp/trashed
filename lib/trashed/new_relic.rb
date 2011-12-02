16907d44ea31bd04e9e6ea1d9f6a6dadf97a0c2c
Jeremy Kemper <jeremy@bitsweat.net>
2011-12-02 10:32:19 -0700

    Sample counter data also

--- lib/trashed/new_relic.rb
+++ lib/trashed/new_relic.rb
@@ -1,29 +1,34 @@
 module Trashed
   module NewRelic
-    def self.sample(meter, options = nil)
+    def self.sample(meter, options = {})
       ::NewRelic::Agent.instance.stats_engine.add_sampler Sampler.new(meter, options)
     end
 
     class Sampler
       attr_accessor :stats_engine
 
-      def initialize(meter, options = nil)
-        @meter = meter
-        @label, @statsd = options.values_at(:label, :statsd) if options
-        @label ||= 'Custom/%s'
+      def initialize(meter, options = {})
+        @meter  = meter
+        @label  = options[:label] || 'Custom/%s'
+        @statsd = options[:statsd]
       end
 
       def poll
-        @meter.gauge.each do |name, value|
-          record_statsd   name, value
+        record @meter.count
+        record @meter.gauge
+      end
+
+      def record(data)
+        data.each do |name, value|
           record_newrelic name, value
+          record_statsd   name, value if @statsd
         end
       end
 
       private
 
       def record_statsd(name, value)
-        @statsd.timing "Performance.#{name}", value if @statsd
+        @statsd.timing "Performance.#{name}", value
       end
 
       def record_newrelic(name, value)
