bb28604b828dd37a4fe165ac28a6bd6552dd6b86
Jeremy Kemper <jeremy@bitsweat.net>
2011-12-03 22:48:40 -0700

    Use OrderedOptions for config. Connect to statsd using config options. Rely on statsd-ruby fork. Bump version to 2.0.4.

--- lib/trashed/railtie.rb
+++ lib/trashed/railtie.rb
@@ -1,28 +1,39 @@
 require 'trashed'
 require 'rails/railtie'
+require 'active_support/ordered_options'
 
 module Trashed
   class Railtie < ::Rails::Railtie
-    class Config < Struct.new(:debug, :logger, :statsd, :sample_rate)
-      def to_hash
-        { :debug => debug, :logger => logger, :statsd => statsd, :sample_rate => sample_rate }
-      end
-    end
-
-    config.trashed = Config.new
+    config.trashed = ActiveSupport::OrderedOptions.new
+    config.trashed.statsd = ActiveSupport::OrderedOptions.new
 
     initializer 'trashed' do |app|
-      config.trashed.logger ||= Rails.logger
+      app.config.trashed.sample_rate ||= 0.1
+      app.config.trashed.statsd = connect_to_statsd(app.config.trashed[:statsd])
+      app.config.trashed.logger = Rails.logger
     end
 
     initializer 'trashed.middleware', :after => 'trashed', :before => 'trashed.newrelic' do |app|
-      app.middleware.insert_after '::Rack::Lock', Trashed::Rack::MeasureResourceUsage, config.trashed.to_hash
+      app.middleware.insert_after '::Rack::Lock', Trashed::Rack::MeasureResourceUsage, app.config.trashed
     end
 
     initializer 'trashed.newrelic', :after => 'newrelic_rpm.start_plugin' do |app|
       if NewRelic::Control.instance.agent_enabled?
         require 'trashed/new_relic'
-        Trashed::NewRelic.sample ResourceUsage, config.trashed.to_hash
+        Trashed::NewRelic.sample ResourceUsage, app.config.trashed
+      end
+    end
+
+    def connect_to_statsd(options)
+      require 'statsd'
+
+      case options
+      when Statsd
+        options
+      when Hash
+        Statsd.new(options[:host], options[:port]).tap do |statsd|
+          statsd.namespace = options[:namespace]
+        end
       end
     end
   end
--- trashed.gemspec
+++ trashed.gemspec
@@ -1,10 +1,12 @@
 Gem::Specification.new do |s|
   s.name      = 'trashed'
-  s.version   = '2.0.3'
+  s.version   = '2.0.4'
   s.author    = 'Jeremy Kemper'
   s.email     = 'jeremy@bitsweat.net'
   s.homepage  = 'https://github.com/37signals/trashed'
   s.summary   = 'Keep tabs on Ruby garbage collection: object counts, allocated bytes, GC time.'
+
+  s.add_dependency 'statsd-ruby', '~> 0.3.1.signals.1'
 
   s.files = Dir["#{File.dirname(__FILE__)}/**/*"]
 end
