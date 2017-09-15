d3dd979345194395b691dd860ac47e91837d07ea
Andrew G10z <agwozdziewycz@salesforce.com>
2017-09-15 16:36:48 -0700

    Address requested feedback.

--- lib/trashed/instruments/backlog.rb
+++ lib/trashed/instruments/backlog.rb
@@ -1,35 +1,31 @@
 module Trashed
   module Instruments
     TCP_ESTABLISHED = 1
-    TCP_SYN_SENT = 2
-    TCP_SYN_RECV = 3
-    TCP_FIN_WAIT1 = 4
-    TCP_FIN_WAIT2 = 5
-    TCP_TIME_WAIT = 6
-    TCP_CLOSE = 7
-    TCP_CLOSE_WAIT = 8
-    TCP_LAST_ACK = 9
-    TCP_LISTEN = 10
-    TCP_CLOSING = 11
+    TCP_SYN_SENT    = 2
+    TCP_SYN_RECV    = 3
+    TCP_FIN_WAIT1   = 4
+    TCP_FIN_WAIT2   = 5
+    TCP_TIME_WAIT   = 6
+    TCP_CLOSE       = 7
+    TCP_CLOSE_WAIT  = 8
+    TCP_LAST_ACK    = 9
+    TCP_LISTEN      = 10
+    TCP_CLOSING     = 11
 
     class ListenBacklog
       def initialize(pid = nil)
-        @pid = nil
-      end
-
-      def start(state, counters, gauges)
+        if pid
+          @basefile = "/proc/net/"
+        else
+          @basefile = "/proc/#{@pid}/net/"
+        end
       end
 
       def measure(state, counters, gauges)
         count = 0
-        if @pid then
-          basefile = "/proc/#{@pid}/"
-        else
-          basefile = "/proc/net/"
-        end
 
         [:tcp, :tcp6].each do |name|
-          ProcTCP.new(File.open(basefile + name)).by(:st => TCP_SYN_RECV) do |entry|
+          ProcTCP.new(File.open(File.join(@basefile, name.to_s)).by(st: TCP_SYN_RECV) do |entry|
             count += 1
           end
         end
@@ -39,9 +35,8 @@ module Trashed
     end
 
     class ProcTCP
-      def initialize(file, pid=nil)
+      def initialize(file)
         @file = file
-        @pid = pid
       end
 
       def by(filters = {})
@@ -62,19 +57,19 @@ module Trashed
         end
 
         @attrs = {
-          :local_addr => toip(pieces[1]),
-          :local_port => pieces[2].hex,
-          :remote_addr => toip(pieces[3]),
-          :remote_port => pieces[4].hex,
-          :st => pieces[5].hex,
-          :tx_queue => pieces[6].to_i,
-          :rx_queue => pieces[7].to_i,
-          :tr => pieces[8].hex,
-          :tm_when => pieces[9],
-          :retrnsmt => pieces[10],
-          :uid => pieces[11].to_i,
-          :timeout => pieces[12].to_i,
-          :inode => pieces[13].to_i,
+          local_addr:  toip(pieces[1]),
+          local_port:  pieces[2].hex,
+          remote_addr: toip(pieces[3]),
+          remote_port: pieces[4].hex,
+          st:          pieces[5].hex,
+          tx_queue:    pieces[6].to_i,
+          rx_queue:    pieces[7].to_i,
+          tr:          pieces[8].hex,
+          tm_when:     pieces[9],
+          retrnsmt:    pieces[10],
+          uid:         pieces[11].to_i,
+          timeout:     pieces[12].to_i,
+          inode:       pieces[13].to_i,
         }
       end
 
@@ -90,13 +85,22 @@ module Trashed
         ret
       end
 
-      private
-
-      def toip(piece)
+      private def toip(piece)
         if piece.length == 8
-          i = piece.hex
+          # An IPv4 address is displayed as a little-endian four-byte
+          # hexadecimal number; that is, the least significant byte is
+          # listed first, so we need to reverse the order of the
+          # bytes to convert it to an IP address.
+          i = piece.hex # convert little endian representation to ruby int.
+
+          # reassemble into IPv4 address by reversing the extracted bytes.
           "#{i & 0xff}.#{(i >> 8) & 0xff}.#{(i >> 16) & 0xff}.#{(i >> 24) & 0xff}"
         else
+          # An IPv6 address is displayed as a little-endian 16-byte
+          # hexadecimal number; that is, the least significant byte is
+          # listed first, so we need to reverse the order of the
+          # bytes to convert it to an IPv6 address. We make no effort
+          # to shorten the length of the address by eliminating 0s.
           parts = piece.scan(/..../).collect do |part|
             part[2..3] + part[0..1]
           end
