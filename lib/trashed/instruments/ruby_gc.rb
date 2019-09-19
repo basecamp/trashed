3b015a7f3183c5cd7e35210205ea6acc49e816ba
Vitali Tsevan <v.tsevan@catawiki.nl>
2019-09-19 11:23:42 +0200

    Move modern rubies measurements into a dedicated constant (DRY)

--- lib/trashed/instruments/ruby_gc.rb
+++ lib/trashed/instruments/ruby_gc.rb
@@ -9,6 +9,11 @@ module Trashed
         :count => :'GC.count'
       }
 
+      RUBY_2X_MEASUREMENTS = {
+        :major_gc_count => :'GC.major_count',
+        :minor_gc_count => :'GC.minor_gc_count'
+      }
+
       # Detect Ruby 1.9, 2.1 or 2.2 GC.stat naming
       begin
         GC.stat :total_allocated_objects
@@ -17,17 +22,17 @@ module Trashed
       rescue ArgumentError
         # Ruby 2.1
         MEASUREMENTS.update \
-          :total_allocated_object => :'GC.allocated_objects',
-          :total_freed_object => :'GC.freed_objects',
-          :major_gc_count => :'GC.major_count',
-          :minor_gc_count => :'GC.minor_gc_count'
+          RUBY_2X_MEASUREMENTS.merge(
+            :total_allocated_object => :'GC.allocated_objects',
+            :total_freed_object => :'GC.freed_objects'
+          )
       else
         # Ruby 2.2+
         MEASUREMENTS.update \
-          :total_allocated_objects => :'GC.allocated_objects',
-          :total_freed_objects => :'GC.freed_objects',
-          :major_gc_count => :'GC.major_count',
-          :minor_gc_count => :'GC.minor_gc_count'
+          RUBY_2X_MEASUREMENTS.merge(
+            :total_allocated_objects => :'GC.allocated_objects',
+            :total_freed_objects => :'GC.freed_objects'
+          )
       end
 
       def measure(state, timings, gauges)
